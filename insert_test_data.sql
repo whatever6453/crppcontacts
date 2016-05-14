6a4d8e471aa89df136dedb4c6867d921d5013b38
whatever6453 <whatever6453@mailinator.com>
2016-05-15 09:43:35 +1000

    Add SQL to insert test data.

---
+++ insert_test_data.sql
@@ -0,0 +1,6 @@
+insert into crppcontacts_contact
+(name, first_name, last_name, organization, position, department, email1, email2, telephone, mobile1, mobile2, country, state, city, street, zip_code, website)
+values
+('Mary Poppins', 'Mary', 'Poppins', 'Nannys Inc', 'Director', 'Childcare', 'poppins@example.com', 'mary@example.com', '+44 1234 5678', '+44 9876 5432', '+44 8765 4321', 'United Kingdom', 'Greater London', 'London', '17 Cherry Tree Lane', 'SE1', 'http://marypoppins.example.com/')
+,('Clark Kent', 'Clark', 'Kent', 'Superman Corp', 'Superhero', 'Villain Control Department', 'superman@example.com', 'clark.kent@example.com', '+1 555 1234 5678', '+1 555 9876 5432', '+1 555 8765 4321', 'United States of America', 'Some State', 'Metropolis', '344 Clinton Street', '00000', 'http://superman.example.com/')
+;
