78ed49bf5da4f3225c5bbaba2423cdb455f8edd3
whatever6453 <whatever6453@mailinator.com>
2016-10-24 21:38:53 +1100

    Permit Django up to version 1.8, and be compatible with that version by explicitly specifying field visibility in serialisers. Allow new versions of Django REST Framework.

--- crppcontacts/serializers.py
+++ crppcontacts/serializers.py
@@ -2,9 +2,13 @@ from crppcontacts.models import Contact, Tag
 from rest_framework import serializers
 
 class ContactSerializer(serializers.HyperlinkedModelSerializer):
+
     class Meta:
         model = Contact
+        fields = ( '__all__' )
 
 class TagSerializer(serializers.HyperlinkedModelSerializer):
+
     class Meta:
         model = Tag
+        fields = ( '__all__' )
