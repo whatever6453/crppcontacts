4ac1e457a7270156feb44d42119cbd83fd9d6e58
whatever6453 <whatever6453@mailinator.com>
2016-10-26 20:45:51 +1100

    Encapsulate the default DataTable arguments in a subclass of it.

--- static/contacts.js
+++ static/contacts.js
@@ -4,6 +4,80 @@ function toTitleCase(str) {
 		return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
 	});
 }
+var RESTfulEditor = function(entityName, columns) {
+	var editor = this, opts = {
+		ajax: function (method, url, oldData, success, error) {
+			var data = {}, pk;
+			pk = editor.DataTablesToDRF(oldData, data);
+			url = '/api/v1/' + entityName + '/';
+			switch (oldData.action) {
+			case 'create':
+				method = 'POST';
+				break;
+			case 'edit':
+				method = 'PUT';
+				url += pk;
+				break;
+			case 'remove':
+				method = 'DELETE';
+				url += pk;
+				break;
+			default:
+				console.log("Unrecognised action '" + data.action + "'");
+				error("Unrecognised action '" + data.action + "'");
+				return;
+			}
+			return $.ajax({
+				type: method,
+				url: url,
+				data: data ? JSON.stringify(data) : undefined,
+				contentType: "application/json",
+				dataType: "json",
+				processData: false,
+				success: function(response) {
+					var editorData = {};
+					editor.DRFToDataTables(response, editorData);
+					return success(editorData);
+				},
+				error: error
+			});
+		},
+		table: '#' + entityName + 'Table',
+		fields: columns,
+		idSrc: 'id'
+	};
+	$.fn.dataTable.Editor.call(this, opts);
+};
+RESTfulEditor.prototype = new $.fn.dataTable.Editor();
+$.extend(RESTfulEditor.prototype, {
+	/**
+	 * Convert from DataTables Editor's pseudo-RESTful API
+	 * to Django REST framework's pseudo-RESTful API.
+	 * @param originalData The data in DataTables Editor JSON format
+	 * @param newData The data in Django REST Framework's JSON format
+	 * @returns The primary key of the entity to operate on
+	 */
+	DataTablesToDRF: function(dtedata, drfdata) {
+		var pk;
+		if (Object.keys(dtedata.data).length > 1) {
+			throw "Cannot operate on more than one entity at a time";
+		}
+		for (pk in dtedata.data) {
+			$.extend(drfdata, dtedata.data[pk]);
+			return pk;
+		}
+		return undefined;
+	},
+	/**
+	 * Convert from Django REST framework's pseudo-RESTful API
+	 * to DataTables Editor's pseudo-RESTful API.
+	 * @param json The data in Django REST Framework's JSON format
+	 */
+	DRFToDataTables: function(drfdata, dtedata) {
+		dtedata.data = [ drfdata ];
+	}
+});
+
 (function($) {
 	$(function() {
 		var columnNames = [
@@ -55,73 +129,7 @@ function toTitleCase(str) {
 				return data.join(', ');
 			}}
 		);
-		/**
-		 * Convert from DataTables' pseudo-RESTful API
-		 * to Django REST framework's pseudo-RESTful API.
-		 * @param originalData The data in DataTables Editor JSON format
-		 * @param newData The data in Django REST Framework's JSON format
-		 * @returns The primary key of the entity to operate on
-		 */
-		function DataTablesToDRF(dtdata, drfdata) {
-			var pk;
-			if (Object.keys(dtdata.data).length > 1) {
-				throw "Cannot operate on more than one entity at a time";
-			}
-			for (pk in dtdata.data) {
-				$.extend(drfdata, dtdata.data[pk]);
-				return pk;
-			}
-			return undefined;
-		}
-		/**
-		 * Convert from Django REST framework's pseudo-RESTful API
-		 * to DataTables' pseudo-RESTful API.
-		 * @param json The data in Django REST Framework's JSON format
-		 */
-		function DRFToDataTables(drfdata, dtdata) {
-			dtdata.data = [ drfdata ];
-		}
-		var editor = new $.fn.dataTable.Editor({
-			ajax: function (method, url, oldData, success, error) {
-				var data = {}, pk;
-				pk = DataTablesToDRF(oldData, data);
-				url = '/api/v1/contacts/';
-				switch (oldData.action) {
-				case 'create':
-					method = 'POST';
-					break;
-				case 'edit':
-					method = 'PUT';
-					url += pk;
-					break;
-				case 'remove':
-					method = 'DELETE';
-					url += pk;
-					break;
-				default:
-					console.log("Unrecognised action '" + data.action + "'");
-					error("Unrecognised action '" + data.action + "'");
-					return;
-				}
-				return $.ajax({
-					type: method,
-					url: url,
-					data: data ? JSON.stringify(data) : undefined,
-					contentType: "application/json",
-					dataType: "json",
-					processData: false,
-					success: function(response) {
-						var editorData = {};
-						DRFToDataTables(response, editorData);
-						return success(editorData);
-					},
-					error: error
-				});
-			},
-			table: '#contactsTable',
-			fields: editorColumns,
-			idSrc: 'id'
-		});
+		var editor = new RESTfulEditor('contacts', editorColumns);
 		$('#contactsTable').DataTable({
 			ajax: {
 				url: '/api/v1/contacts',
