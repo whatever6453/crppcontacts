97edadab2ce211b4f7fea01133877b24b31d70b1
whatever6453 <whatever6453@mailinator.com>
2016-10-26 17:51:37 +1100

    Be briefer in imports.

--- crppcontacts/models.py
+++ crppcontacts/models.py
@@ -1,8 +1,7 @@
-# from django.db import models
-import django.db.models
+from django.db import models
 
 
-class Common(django.db.models.Model):
+class Common(models.Model):
     """
     Abstract class for common attributes and behaviour
     """
@@ -14,7 +13,7 @@ class Tag(Common):
     """
     Represents a TAG to categorize contacts
     """
-    name = django.db.models.CharField('Tag name', max_length=50, null=False, blank=False)
+    name = models.CharField('Tag name', max_length=50, null=False, blank=False)
     def __unicode__(self):  # Python 3: def __str__(self):
         return self.name
 
@@ -23,22 +22,22 @@ class Contact(Common):
     """
     Represents a contact
     """
-    first_name = django.db.models.CharField('First name', max_length=50, null=True, blank=True)
-    last_name = django.db.models.CharField('Last name', max_length=100, null=True, blank=True)
-    organization = django.db.models.CharField('Organization', max_length=200, null=True, blank=True)
-    position = django.db.models.CharField('Position', max_length=200, null=True, blank=True)
-    department = django.db.models.CharField('Department', max_length=200, null=True, blank=True)
-    email1 = django.db.models.EmailField('Email', max_length=250, null=True, unique=False, blank=True)
-    email2 = django.db.models.EmailField('Alternate email', max_length=250, null=True, unique=False, blank=True)
-    telephone = django.db.models.CharField('Telephone', max_length=50, null=True, blank=True)
-    mobile1 = django.db.models.CharField('Mobile phone', max_length=50, null=True, blank=True)
-    mobile2 = django.db.models.CharField('Alternate mobile phone', max_length=50, null=True, blank=True)
-    country = django.db.models.CharField('Country', max_length=50, null=True, blank=True)
-    state = django.db.models.CharField('State', max_length=50, null=True, blank=True)
-    city = django.db.models.CharField('City', max_length=50, null=True, blank=True)
-    street = django.db.models.CharField('Street address', max_length=200, null=True, blank=True)
-    zip_code = django.db.models.CharField('Zip code', max_length=50, null=True, blank=True)
-    website = django.db.models.CharField('Web site URL', max_length=200, null=True, blank=True)
-    tags = django.db.models.ManyToManyField(Tag)
+    first_name = models.CharField('First name', max_length=50, null=True, blank=True)
+    last_name = models.CharField('Last name', max_length=100, null=True, blank=True)
+    organization = models.CharField('Organization', max_length=200, null=True, blank=True)
+    position = models.CharField('Position', max_length=200, null=True, blank=True)
+    department = models.CharField('Department', max_length=200, null=True, blank=True)
+    email1 = models.EmailField('Email', max_length=250, null=True, unique=False, blank=True)
+    email2 = models.EmailField('Alternate email', max_length=250, null=True, unique=False, blank=True)
+    telephone = models.CharField('Telephone', max_length=50, null=True, blank=True)
+    mobile1 = models.CharField('Mobile phone', max_length=50, null=True, blank=True)
+    mobile2 = models.CharField('Alternate mobile phone', max_length=50, null=True, blank=True)
+    country = models.CharField('Country', max_length=50, null=True, blank=True)
+    state = models.CharField('State', max_length=50, null=True, blank=True)
+    city = models.CharField('City', max_length=50, null=True, blank=True)
+    street = models.CharField('Street address', max_length=200, null=True, blank=True)
+    zip_code = models.CharField('Zip code', max_length=50, null=True, blank=True)
+    website = models.CharField('Web site URL', max_length=200, null=True, blank=True)
+    tags = models.ManyToManyField(Tag)
     def __unicode__(self):  # Python 3: def __str__(self):
         return self.first_name + ' ' + self.last_name
